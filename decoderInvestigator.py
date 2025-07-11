76369a85b0806e9dc7fd46b10e586a3c475765f0
jhessionkunz-arcadia <88674613+jhessionkunz-arcadia@users.noreply.github.com>
2025-07-11 14:44:18 -0400

    Removing unnecessary files
    Removing unnecessary files

--- decoderInvestigator.py
+++
@@ -1,5 +0,0 @@
-with open('dataOutput/blockGroup-99vars-export-3-20241217-193739.csv', 'rb') as f:
-    data = f.read()
-
-problem_byte = data[4708]
-print(f"Byte at position 4708: {hex(problem_byte)}")
